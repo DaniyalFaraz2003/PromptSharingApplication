b3bfee7395af473314cf6fa2c84983e7ab3e48e8
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2025-01-17 21:25:32 +0500

    implmenting nextauth

--- promptsharingclient/components/Navbar.tsx
+++ promptsharingclient/components/Navbar.tsx
@@ -4,7 +4,8 @@ import logo from "@/public/logo.png";
 import Link from 'next/link';
 
 export const Navbar = () => {
-
+    const loggedIn = false;
+    const username = 'John Doe';
     return (
         <nav className='flex p-3 items-center w-full bg-transparent text-black shadow-sm' role='navigation'>
             <div className='flex items-center space-x-2'>
@@ -15,14 +16,28 @@ export const Navbar = () => {
                     <a href='/' className='text-2xl/7 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-orange-700 via-orange-400 to-yellow-400 sm:truncate sm:text-3xl sm:tracking-tight'>SharePrompt</a>
                 </h1>
             </div>
-            <div className='flex items-center space-x-4 ml-auto'>
-                <Link href='/auth/login'>
-                    <button className="btn btn-outline-warning  border-orange-400 text-orange-400 hover:bg-orange-400 font-bold">Login</button>
-                </Link>
-                <Link href='/auth/signup'>
-                    <button className="btn btn-warning  bg-orange-400 text-white font-bold">Sign Up</button>
-                </Link>
-            </div>
+            {!loggedIn && (
+                <div className='flex items-center space-x-4 ml-auto'>
+                    <Link href='/auth/login'>
+                        <button className="btn btn-outline-warning  border-orange-400 text-orange-400 hover:bg-orange-400 font-bold">Login</button>
+                    </Link>
+                    <Link href='/auth/signup'>
+                        <button className="btn btn-warning  bg-orange-400 text-white font-bold">Sign Up</button>
+                    </Link>
+                </div>
+            )}
+
+            {loggedIn && (
+                <div className='flex items-center space-x-4 ml-auto'>
+                    <h1 className='font-bold text-xl text-black'>{username}</h1>
+                    <div className="popover">
+                        <label className="popover-trigger btn p-3 text-white bg-gray-600 rounded-full" tabIndex={0}>DF</label>
+                        <div className="popover-content popover-bottom-left w-32" tabIndex={0}>
+                            <button className="btn btn-warning bg-orange-400 text-white font-bold">Log Out</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </nav>
     );
 }
---
+++ promptsharingclient/db/user.ts
@@ -0,0 +1,31 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required!'],
+        unique: [true, 'Email already exists!'],
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Email invalid, it should be a valid email address!"]
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required!'],
+        unique: [true, 'Username already exists!'],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!'],
+        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, "Password invalid, it should contain at least 8 characters, one uppercase letter, one lowercase letter, and one number!"]
+    },
+    image: {
+        type: String,
+    }
+})
+
+const User = models.User || model('User', UserSchema);
+export default User;
