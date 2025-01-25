6d7576227687467e1538abbe2649fac7014b8796
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2025-01-25 16:41:32 +0500

    some progress

--- promptsharingapp/src/main/java/com/promptsharer/promptsharingapp/config/WebConfig.java
+++ promptsharingapp/src/main/java/com/promptsharer/promptsharingapp/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.promptsharer.promptsharingapp.config;
 
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,16 +15,30 @@ public class WebConfig implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        // Handle the preflight OPTIONS request
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+            res.setHeader("Access-Control-Max-Age", "3600");
+            res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, x-requested-with, Cache-Control");
+            res.setStatus(HttpServletResponse.SC_OK); // Make sure to return 200 OK for OPTIONS request
+            return; // Stop further processing of this request
+        }
+
+        // Continue processing the request for other HTTP methods
         res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         res.setHeader("Access-Control-Max-Age", "3600");
         res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, x-requested-with, Cache-Control");
-        res.setStatus(HttpServletResponse.SC_OK);
-        chain.doFilter(request, res);
+        chain.doFilter(request, res); // Continue with the filter chain
     }
+
     @Override
     public void destroy() {
     }
